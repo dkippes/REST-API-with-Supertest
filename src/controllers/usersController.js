1802b07046642a9c109fcd028839574c363a2eab
dkippes <kippes.diego@gmail.com>
2021-03-19 20:43:04 -0300

    Added: - Database folder

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,20 +1,4 @@
-const users = [
-	{
-		id: 1,
-		username: 'diego',
-		password: 1234
-	},
-	{
-		id: 2,
-		username: 'ivan',
-		password: 4321
-	},
-	{
-		id: 3,
-		username: 'kippes',
-		password: 4567
-	}
-]
+const users = require('../database/users');
 
 const usersController = {
 	getAllUsers: (req, res) => {
---
+++ src/database/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+	{
+		id: 1,
+		username: 'diego',
+		password: 1234
+	},
+	{
+		id: 2,
+		username: 'ivan',
+		password: 4321
+	},
+	{
+		id: 3,
+		username: 'kippes',
+		password: 4567
+	}
+]
